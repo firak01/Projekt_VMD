08a5bee2b9a9f4c72985ce1aa7c27c0df46cd73f
firak01 <firak01@t-online.de>
2015-04-23 08:43:41 +0200

    Umgebungsvariablen in Ant und mit Flaka
    Hier insbesondere die Expression Language Syntax (EL) von Flaka
    ausprobiert.

---
+++ bin/tryoutAnt_FlakaEnvironmentVariable.bat
@@ -0,0 +1,14 @@
+REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
+REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
+echo start tryoutAnt_FlakaEnvironmentVariable
+
+REM Wir wollen hier keine Java Files builden
+REM call ant -buildfile testFlaka_Patton.xml
+
+REM Wir wollen hier ggfs. Dateien verarbeiten
+SET TRYOUT_ANT="Das ist ein Test"
+call ant -f tryoutAnt_FlakaEnvironmentVariable.xml
+
+echo Ende tryoutAnt_FlakaEnvironmentVariable
+pause
+REM timeout /T 20 /nobreak
