f7ccbf1f5e1ea3a2bda63c5fbae91c5596ace2ef
firak01 <firak01@t-online.de>
2015-05-06 08:43:55 +0200

    Idee des Mappens der VM zu den Verzeichnissen auf dem Host

---
+++ bin/1_VmdByFGL_copyRepository2Local_HOST.bat
@@ -0,0 +1,9 @@
+REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
+REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
+echo start Copy Local to Repository HOST
+
+call ant -buildfile ..\src\VMDbyFGL_HostChangesPush.xml
+
+echo Ende Copy Local to Repository HOST
+pause
+REM timeout /T 20 /nobreak
--- bin/2_VmdByFGL_copyLocal2Repository_HOST.bat
+++ bin/2_VmdByFGL_copyLocal2Repository_HOST.bat
@@ -1,9 +1,9 @@
 REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
 REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
-echo start Copy Client to Export HOST
+echo start Copy Repository to Local HOST
 
-call ant -buildfile ..\src\VMDbyFGL_HostChangesPush.xml
+call ant -buildfile ..\src\VMDbyFGL_HostChangesPull.xml
 
-echo Ende Copy Client to Export HOST
+echo Ende Copy Repository to Local HOST
 pause
 REM timeout /T 20 /nobreak
