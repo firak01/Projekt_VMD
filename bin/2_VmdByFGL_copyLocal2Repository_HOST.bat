9b98dd149d7d29ac2b97149ccbca186b15c9e7dc
firak01 <firak01@t-online.de>
2015-05-07 08:20:24 +0200

    Berechnung des Dateinamens. GOON: Backup Misc - Verzeichnis

--- bin/2_VmdByFGL_copyLocal2Repository_HOST.bat
+++ bin/2_VmdByFGL_copyLocal2Repository_HOST.bat
@@ -2,7 +2,7 @@ REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert i
 REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
 echo start Copy Repository to Local HOST
 
-call ant -buildfile ..\src\VMDbyFGL_HostChangesPull.xml
+call ant -buildfile ..\src\VMDbyFGL_HostChangesPush.xml
 
 echo Ende Copy Repository to Local HOST
 pause
