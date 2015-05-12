10b3c08b2cabaab1660f83dfcce718cef08faf1f
firak01 <firak01@t-online.de>
2015-05-12 08:54:09 +0200

    TryOut von ant-contrib zum Parsen von Textdateien

---
+++ test/tryoutAnt_ContribFileProcessing.bat
@@ -0,0 +1,15 @@
+REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
+REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
+echo start tryoutAnt_ContribFileProcessing
+
+REM Wir wollen hier keine Java Files builden
+REM call ant -buildfile testFlaka_Patton.xml
+
+REM Wir wollen hier nicht auf umgebungsvariablen zugreifen SET TRYOUT_ANT="Das ist ein Test"
+
+REM Wir wollen hier ggfs. Dateien verarbeiten
+call ant -f tryoutAnt_ContribFileProcessing.xml
+
+echo Ende tryoutAnt_ContribFileProcessing
+pause
+REM timeout /T 20 /nobreak
