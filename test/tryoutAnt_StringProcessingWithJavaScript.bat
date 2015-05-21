d8ac95ae5d319d65f400487da92145fa24e4dd60
firak01 <firak01@t-online.de>
2015-05-21 08:50:57 +0200

    Weiteres zur Stringverarbeitung: Substring
    TODO GOON: Weiter am lokalen Kopieren arbeiten.

---
+++ test/tryoutAnt_StringProcessingWithJavaScript.bat
@@ -0,0 +1,14 @@
+REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
+REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
+echo start tryoutAnt_StringProcessingWithJavaScript
+
+REM Wir wollen hier keine Java Files builden
+REM call ant -buildfile testFlaka_Patton.xml
+
+REM Wir wollen hier nicht auf umgebungsvariablen zugreifen SET TRYOUT_ANT="Das ist ein Test"
+
+call ant -f tryoutAnt_StringProcessingWithJavaScript.xml
+
+echo Ende tryoutAnt_StringProcessingWithJavaScript
+pause
+REM timeout /T 20 /nobreak
