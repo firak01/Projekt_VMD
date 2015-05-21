2ef73ace6b767814889f69e3d2d7327b3b02f34e
firak01 <firak01@t-online.de>
2015-05-21 18:41:49 +0200

    Zwischenschritt: Umgebungsvariablen vs Eclipse Arguments

--- bin/2_VmdByFGL_copyLocal2Repository_HOST.bat
+++ bin/2_VmdByFGL_copyLocal2Repository_HOST.bat
@@ -1,10 +1,11 @@
-@echo off
+﻿@echo off
 REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
 REM Das nur unter Windows DOS Box aufrufen, unter Eclipse werden die Pfade nicht gefunden.
 REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
 echo start Copy Repository to Local HOST
 
-SET vmd=C:/1fgl/repository/Projekt_VMD
+SET TRYOUT_ANT="Das ist ein Test"
+SET VMD="C:/1fgl/repository/Projekt_VMD"
 call ant -buildfile ..\src\VMDbyFGL_HostChangesPush.xml
 
 echo Ende Copy Repository to Local HOST
