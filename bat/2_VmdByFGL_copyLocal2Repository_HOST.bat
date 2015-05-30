92bb0b4314ea29b9a0805bd5cf88245eb9a12ddb
firak01 <firak01@t-online.de>
2015-05-30 08:55:12 +0200

    Überrpüfung auf isdirectory mit neuem Scriptdef

--- bat/2_VmdByFGL_copyLocal2Repository_HOST.bat
+++ bat/2_VmdByFGL_copyLocal2Repository_HOST.bat
@@ -1,13 +1,15 @@
-ï»¿@echo off
+@echo off
+REM KONVERTIERE DIESE DATEI ZU ANSI, NUR DANN WIRD '@echo off' als Befehl erkannt.
+
 REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
 REM Das nur unter Windows DOS Box aufrufen, unter Eclipse werden die Pfade nicht gefunden.
-REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgefÃ¼hrt.
+REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
 echo start Copy Repository to Local HOST
 
-REM Varinte A) FÃ¼r Batch Alternative MÃ¶glichkeit Ã¼ber Umgebungsvariablen Parameter zu Ã¼bergeben.
+REM Varinte A) Für Batch Alternative Möglichkeit über Umgebungsvariablen Parameter zu übergeben.
 SET TRYOUT_ANT="Das ist ein Test"
 SET VMD="C:/1fgl/repository/Projekt_VMD"
-REM Variante B) Sowohl fÃ¼r Eclipse (Run Konfiguration) als auch Batch. 
+REM Variante B) Sowohl für Eclipse (Run Konfiguration) als auch Batch. 
 REM MERKE: Wenn die Batch auch aus Eclipse heraus gestartet werden soll, braucht man wohl ANT_HOME usw. als Umgebungsvariable. 
 
 call ant -buildfile ..\src\VMDbyFGL_HostChangesPush.xml -Dvmd=C:/1fgl/repository/Projekt_VMD/bat/projekt_vmd.properties
