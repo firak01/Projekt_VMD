4f78408a35d07262769e8c0462b7601ad80e035d
firak01 <firak01@t-online.de>
2015-06-07 17:17:20 +0200

    weiter am ListHandling --- TODO: Entwickle das Anhängen, Indexzugriff, usw.

---
+++ test/tryoutAnt_FlakaListHandling.bat
@@ -0,0 +1,17 @@
+@echo off
+REM KONVERTIERE DIESE DATEI ZU ANSI, NUR DANN WIRD '@echo off' als Befehl erkannt.
+
+REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
+REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
+echo start tryoutAnt_FlakaListHandling
+
+REM Wir wollen hier keine Java Files builden
+REM call ant -buildfile testFlaka_Patton.xml
+
+REM Wir wollen hier ggfs. Dateien verarbeiten
+SET TRYOUT_ANT="Das ist ein Test"
+call ant -f tryoutAnt_FlakaListHandling.xml
+
+echo Ende tryoutAnt_FlakaListHandling
+pause
+REM timeout /T 20 /nobreak
