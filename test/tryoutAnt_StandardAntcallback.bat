3c4d5212fda9476defd20a9729e861005344b1b6
firak01 <firak01@t-online.de>
2015-07-08 11:38:49 +0200

    11
    Aufruf interner targets mit einem führenden Minuszeichen, dadurch sind
    sie von aussen nicht aufrufbar.

---
+++ test/tryoutAnt_StandardAntcallback.bat
@@ -0,0 +1,18 @@
+@echo off
+REM KONVERTIERE DIESE DATEI ZU ANSI, NUR DANN WIRD '@echo off' als Befehl erkannt.
+
+REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
+REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
+echo start tryoutAnt_StandardAntcallback
+
+REM Wir wollen hier keine Java Files builden
+REM call ant -buildfile testFlaka_Patton.xml
+
+REM Wir wollen hier nicht auf umgebungsvariablen zugreifen SET TRYOUT_ANT="Das ist ein Test"
+
+REM Wir wollen hier ggfs. Dateien verarbeiten
+call ant -f tryoutAnt_StandardAntcallback.xml
+
+echo Ende tryoutAnt_StandardAntcallback
+pause
+REM timeout /T 20 /nobreak
