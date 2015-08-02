4d4d47fc71071aa4eeca8c5d7f2d51b544dd698c
firak01 <firak01@t-online.de>
2015-08-02 09:37:11 +0200

    Lokale Projektdatei mit HOST spezifischen Namen als Uebersteuerung

--- bat/2_VmdByFGL_copyLocal2Repository_HOST.bat
+++ bat/2_VmdByFGL_copyLocal2Repository_HOST.bat
@@ -6,12 +6,17 @@ REM Das nur unter Windows DOS Box aufrufen, unter Eclipse werden die Pfade nicht
 REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
 echo start Copy Repository to Local HOST
 
+
+REM Eine Startdatei mit den Projekteinstellungen übergeben. 
+REM Merke: Der übergebenen Dateiname wird ggfs. durch die existenz eines Dateinamens project_ "Hostname des Rechners" _vmd.properties übersteuert.
+REM Merke: Die Pfadangaben sind mit Slash und nicht mit Backslash
+
 REM Varinte A) Für Batch Alternative Möglichkeit über Umgebungsvariablen Parameter zu übergeben.
 SET TRYOUT_ANT="Das ist ein Test"
 SET VMD="C:/1fgl/repository/Projekt_VMD"
+
 REM Variante B) Sowohl für Eclipse (Run Konfiguration) als auch Batch. 
 REM MERKE: Wenn die Batch auch aus Eclipse heraus gestartet werden soll, braucht man wohl ANT_HOME usw. als Umgebungsvariable. 
-
 call ant -buildfile ..\src\VMDbyFGL_HostChangesPush.xml -Dvmd=C:/1fgl/repository/Projekt_VMD/bat/projekt_vmd.properties
 
 echo Ende Copy Repository to Local HOST
