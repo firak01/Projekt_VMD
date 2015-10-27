eabc2718a91acc0a075df21c355436b9a12b3738
Firak01 <firak01@t-online.de>
2015-10-27 08:06:55 +0100

    enableJava() - JavaScript Funktion, um auch unter Java 8 (Nashorn) die Java 7 (Rhino) Methode importPackage bereitzustellen.
    Signed-off-by: Firak01 <firak01@t-online.de>

--- test/tryoutAnt_ConditionHandlingWith_Flaka_Contrib.bat
+++ test/tryoutAnt_ConditionHandlingWith_Flaka_Contrib.bat
@@ -3,7 +3,7 @@ REM KONVERTIERE DIESE DATEI ZU ANSI, NUR DANN WIRD '@echo off' als Befehl erkann
 
 REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
 REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgefuehrt.
-echo start tryoutAnt_StringProcessingWithJavaScript
+echo start tryoutAnt_ConditionHandlingWith_Flaka_Contrib
 
 REM Wir wollen hier keine Java Files builden
 REM call ant -buildfile testFlaka_Patton.xml
@@ -12,6 +12,6 @@ REM Wir wollen hier nicht auf umgebungsvariablen zugreifen SET TRYOUT_ANT="Das i
 
 call ant -f tryoutAnt_ConditionHandlingWith_Flaka_Contrib.xml
 
-echo Ende tryoutAnt_StringProcessingWithJavaScript
+echo Ende tryoutAnt_ConditionHandlingWith_Flaka_Contrib
 pause
 REM timeout /T 20 /nobreak
