65dd0cfcaff4d53553a6c91680dedbdc75e5c708
firak01 <firak01@t-online.de>
2016-01-21 09:07:58 +0100

    5

--- test/tryoutAnt_Contrib-FlakaStringProcessing.bat
+++ test/tryoutAnt_Contrib-FlakaStringProcessing.bat
@@ -3,15 +3,15 @@ REM KONVERTIERE DIESE DATEI ZU ANSI, NUR DANN WIRD '@echo off' als Befehl erkann
 
 REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
 REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
-echo start tryoutAnt_ContribStringProcessing
+echo start tryoutAnt_Contrib-FlakaStringProcessing
 REM Wir wollen hier keine Java Files builden
 REM call ant -buildfile testFlaka_Patton.xml
 
 REM Wir wollen hier nicht auf umgebungsvariablen zugreifen SET TRYOUT_ANT="Das ist ein Test"
 
 REM Wir wollen hier ggfs. Dateien verarbeiten
-call ant -f tryoutAnt_ContribStringProcessing.xml
+call ant -f tryoutAnt_Contrib-FlakaStringProcessing.xml
 
-echo Ende tryoutAnt_ContribStringProcessing
+echo Ende tryoutAnt_Contrib-FlakaStringProcessing
 pause
 REM timeout /T 20 /nobreak
