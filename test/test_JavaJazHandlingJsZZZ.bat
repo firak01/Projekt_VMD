c0a733a47c5dade60979b4b23d3126f775356aa0
firak01 <firak01@t-online.de>
2016-02-22 12:44:46 +0100

    7

--- test/test_JavaJazHandlingJsZZZ.bat
+++ test/test_JavaJazHandlingJsZZZ.bat
@@ -3,15 +3,14 @@ REM KONVERTIERE DIESE DATEI ZU ANSI, NUR DANN WIRD '@echo off' als Befehl erkann
 
 REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
 REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
-echo start tryoutAnt_JAZUsedByJavaScript
-
+echo start test_JavaJazHandlingJsZZZ.bat
 REM Wir wollen hier keine Java Files builden
 REM call ant -buildfile testFlaka_Patton.xml
 
 REM Wir wollen hier nicht auf umgebungsvariablen zugreifen SET TRYOUT_ANT="Das ist ein Test"
 
-call ant -f tryoutAnt_JAZUsedByJavaScript.xml
+call ant -f test_JavaJazHandlingJsZZZ.xml
 
-echo Ende tryoutAnt_JAZUsedByJavaScript
+echo Ende test_JavaJazHandlingJsZZZ.bat
 pause
 REM timeout /T 20 /nobreak
