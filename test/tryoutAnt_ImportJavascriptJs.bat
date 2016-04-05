e72c655eaf387471d4ac957c5c1f6b402047406f
firak01 <firak01@t-online.de>
2016-04-05 09:17:48 +0200

    13 TODO Fehler mit Java8 testen und dann importierte Datei erst zur Verfügung stellen.

---
+++ test/tryoutAnt_ImportJavascriptJs.bat
@@ -0,0 +1,17 @@
+@echo off
+REM KONVERTIERE DIESE DATEI ZU ANSI, NUR DANN WIRD '@echo off' als Befehl erkannt.
+CLS
+
+REM Verwendet wird ant. Voraussetzung ist also das Java Ant - Tool installiert ist.
+REM Unbedingt mit call aufrufen, sonst werden nachfolgende Anweisungen nicht mehr ausgeführt.
+echo start tryoutAnt_ImportJavascriptJs.bat
+REM Wir wollen hier keine Java Files builden
+REM call ant -buildfile testFlaka_Patton.xml
+
+REM Wir wollen hier nicht auf umgebungsvariablen zugreifen SET TRYOUT_ANT="Das ist ein Test"
+
+call ant -f tryoutAnt_ImportJavascriptJs.xml
+
+echo Ende tryoutAnt_ImportJavascriptJs.bat
+pause
+REM timeout /T 20 /nobreak
