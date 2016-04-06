633d8a540284b37d23d32ade002388d9d64ca14d
firak01 <firak01@t-online.de>
2016-04-06 08:17:06 +0200

    16 Man kann erfolgreich externe Dateien einbinden.

---
+++ test/tryoutAnt_ImportJavascriptJs.js
@@ -0,0 +1,13 @@
+function tryoutAnt_ImportJavascript(myValue){
+	print("################################");
+	print("### START: externes JavaScript ###");
+	print("################+++++###########");
+	
+	
+	print("emfangener Parameter: " + myValue);
+	
+	
+	print("################################");
+	print("### ENDE: externes JavaScript ###");
+	print("################+++++###########");
+}
