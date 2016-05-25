49a036954d792cdcb4ec04608d494bd55f5aeae6
firak01 <firak01@t-online.de>
2016-05-25 08:20:15 +0200

    11 arrayAppend

--- jsscriptdef/ZAnt.basic.zBasic.util.DatatypeZZZ.js
+++ jsscriptdef/ZAnt.basic.zBasic.util.DatatypeZZZ.js
@@ -4,6 +4,18 @@
 	function isNumber(n) {
 			return !isNaN(parseFloat(n)) && isFinite(n);
 	}
+	
+	function encodeMyHtml(htmlToEncode) {
+		//TODO: In den JSZ Kernel bringen
+		 var encodedHtml = escape(htmlToEncode);
+		 encodedHtml = encodedHtml.replace(/\//g,"%2F");
+		 encodedHtml = encodedHtml.replace(/\?/g,"%3F");
+		 encodedHtml = encodedHtml.replace(/=/g,"%3D");
+		 encodedHtml = encodedHtml.replace(/&/g,"%26");
+		 encodedHtml = encodedHtml.replace(/@/g,"%40");
+		 return encodedHtml;
+		 //merke: mit unescape(...) bekommt man den korrekten String wieder zurück.
+	   }
 			
 			
 			
