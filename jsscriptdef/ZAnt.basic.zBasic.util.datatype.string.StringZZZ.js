9c2f4583c6b3adf5b69a1381f0f9647fe3eba2a7
firak01 <firak01@t-online.de>
2016-06-08 10:36:58 +0200

    stringLeftStringJsZZZ stringExplodeJsZZZ

--- jsscriptdef/ZAnt.basic.zBasic.util.datatype.string.StringZZZ.js
+++ jsscriptdef/ZAnt.basic.zBasic.util.datatype.string.StringZZZ.js
@@ -2,6 +2,19 @@
 //### STRING HANDLING
 //###############################################################
 //äquivalent zu JSZ - Kernel
+
+//TODO: In den JSZ Kernel bringen
+function encodeMyHtml(htmlToEncode) {
+	var encodedHtml = escape(htmlToEncode);
+	encodedHtml = encodedHtml.replace(/\//g,"%2F");
+	encodedHtml = encodedHtml.replace(/\?/g,"%3F");
+	encodedHtml = encodedHtml.replace(/=/g,"%3D");
+	encodedHtml = encodedHtml.replace(/&/g,"%26");
+	encodedHtml = encodedHtml.replace(/@/g,"%40");
+	return encodedHtml;
+	//merke: mit unescape(...) bekommt man den korrekten String wieder zurück.
+}
+
 //@LeftBack equivalent, !!! casesensitive
 function getStrLeftBackStr(sourceStr,keyStr,objControl){
 	var sReturn=""; 
